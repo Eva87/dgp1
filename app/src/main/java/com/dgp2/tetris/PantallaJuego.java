5dcbbb88d0e35eff746bf169dc693ebc8460b95e
eva <eva.1987@gmail.com>
2020-01-23 14:47:37 +0100

    Botones y muy basico sin funcionalidad mas que el paso del main al juego

--- app/src/main/java/com/dgp2/tetris/PantallaJuego.java
+++ app/src/main/java/com/dgp2/tetris/PantallaJuego.java
@@ -6,31 +6,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private ImageButton botonplay;
+public class PantallaJuego extends AppCompatActivity {
+
+    private ImageButton izq;
+    private ImageButton der;
+    private ImageButton abajo;
+    private ImageButton girar;
+    private TextView puntos;
     private Intent intent=null;
     private long tiempoesperavolver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_pantalla_juego);
         initViews();
     }
 
 
 
-    public void iniciarJuego(View view){
-        intent = new Intent(MainActivity.this, PantallaJuego.class);
-        startActivity(intent);
-        finish();
+    public void moverizq(View view){
+
+
+    }
+    public void moverder(View view){
+
+
+    }
+    public void moverabajo(View view){
+
+
+    }
+    public void giropieza(View view){
+
+
     }
 
     private void initViews(){
-        botonplay = findViewById(R.id.botonPlay);
+        izq = findViewById(R.id.Izq);
+        der = findViewById(R.id.Der);
+        abajo = findViewById(R.id.Abajo);
+        girar = findViewById(R.id.Giro);
+        puntos = findViewById(R.id.puntos);
     }
 
     public void onBackPressed(){
